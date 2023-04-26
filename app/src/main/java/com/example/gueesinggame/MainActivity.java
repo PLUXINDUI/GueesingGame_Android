456a5a249be24e8e883c53af663e6c559d300075
PLUXINDUI <alexsun1503@gmail.com>
2023-04-26 16:52:43 +0300

    выв

---
+++ app/src/main/java/com/example/gueesinggame/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.gueesinggame;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.KeyEvent;
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.gueesinggame.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtGuess;
+    private Button btnGuess;
+    private int numberOfTries = 1;
+    private TextView lblOutput;
+    private int theNumber;
+    public void checkGuess() {
+        String guessText = txtGuess.getText().toString();
+        String message = "";
+        try {
+            int guess = Integer.parseInt(guessText);
+            if (guess < theNumber)
+                message = guess + " is too low. Try again.";
+            else if (guess > theNumber)
+                message = guess + " is too high. Try again";
+            else {
+                message = guess + " is correct. You win after" + numberOfTries + "tries!";
+            }
+        } catch (Exception e) {
+            message = "Enter a whole number between 1 and 100.";
+        } finally {
+
+            lblOutput.setText(message);
+            txtGuess.requestFocus();
+            txtGuess.selectAll();
+            numberOfTries ++;
+        }
+    }
+    public void newGame() {
+        theNumber = (int) (Math.random() * 100 + 1);
+    }
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        txtGuess = (EditText) findViewById(R.id.txtGuess);
+        btnGuess = (Button) findViewById(R.id.btnGuess);
+        lblOutput = (TextView) findViewById(R.id.lblOutput);
+
+        newGame();
+
+        btnGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            checkGuess();
+            }
+        });
+
+        txtGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                checkGuess();
+                return true;
+            }
+        });
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_graph);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_graph);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
