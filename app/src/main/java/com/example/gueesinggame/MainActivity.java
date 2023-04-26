ffa0c766a2d438386ecaa858e1cfcd6b6dd6e97a
PLUXINDUI <alexsun1503@gmail.com>
2023-04-26 22:17:33 +0300

    fixed

--- app/src/main/java/com/example/gueesinggame/MainActivity.java
+++ app/src/main/java/com/example/gueesinggame/MainActivity.java
@@ -41,16 +41,15 @@ public class MainActivity extends AppCompatActivity {
             else if (guess > theNumber)
                 message = guess + " is too high. Try again";
             else {
-                message = guess + " is correct. You win after" + numberOfTries + "tries!";
+                message = guess + " is correct. You win after " + numberOfTries + " tries!";
             }
         } catch (Exception e) {
             message = "Enter a whole number between 1 and 100.";
         } finally {
-
             lblOutput.setText(message);
             txtGuess.requestFocus();
             txtGuess.selectAll();
-            numberOfTries ++;
+            numberOfTries += 1;
         }
     }
     public void newGame() {
@@ -75,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         btnGuess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            checkGuess();
+                checkGuess();
             }
         });
 
@@ -86,49 +85,5 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_graph);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_graph);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
